0e7ad80e8b7fae26e61d88693769a19added066e
rajvlog <70760269+rajvlog@users.noreply.github.com>
2022-10-02 21:30:51 +0530

    Day2 progress

---
+++ DAY2/Sol.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Sol {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length()+B.length());
+            if(A.compareTo(B)<=0)System.out.println("No");
+            else System.out.println("Yes");
+            System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1)+" "+B.substring(0, 1).toUpperCase() + B.substring(1));
+        }
+    }
+
+
+
