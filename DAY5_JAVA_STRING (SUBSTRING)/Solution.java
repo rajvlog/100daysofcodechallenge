fecdfc6ef0365a30c1a77bcf0d0c6a68e4b794ee
rajvlog <70760269+rajvlog@users.noreply.github.com>
2022-10-05 23:05:14 +0530

    Add files via upload

---
+++ DAY5_JAVA_STRING (SUBSTRING)/Solution.java
@@ -0,0 +1,24 @@
+/*Given a string, s, and two indices, start and end, print a substring consisting of
+ all characters in the inclusive range from start to end – 1. You’ll find the String class’ substring
+ method helpful in completing this challenge.
+
+Input Format
+The first line contains a single string denoting s.
+The second line contains two space-separated integers denoting the respective values of start and end.*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.print(S.substring(start,end));
+    }
+}
