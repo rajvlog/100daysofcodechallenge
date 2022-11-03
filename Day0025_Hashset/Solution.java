2fc8c5a6b59cf4f04fd37773995ba4e552bfd6dd
rajvlog <70760269+rajvlog@users.noreply.github.com>
2022-11-03 21:42:57 +0530

    Add files via upload

---
+++ Day0025_Hashset/Solution.java
@@ -0,0 +1,31 @@
+/*Input Format
+
+In the first line, there will be an integer T denoting number of pairs.
+ Each of the next T lines will contain two strings seperated by a single space.*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+HashSet<String> set = new HashSet(t);
+        for (int i = 0; i < t; i++) {
+            set.add(pair_left[i] + " " + pair_right[i]);
+            System.out.println(set.size());
+        }
+
+    }
+}
