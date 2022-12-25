2f2bbae80efe9c56eba0d898e023c2c419806fcc
rajvlog <70760269+rajvlog@users.noreply.github.com>
2022-12-25 16:57:59 +0530

    Day 63 progress

---
+++ Day0063_Remove_Element/Solution.java
@@ -0,0 +1,22 @@
+/*Given an integer array nums and an integer val,
+ remove all occurrences of val in nums in-place.
+ The relative order of the elements may be changed.
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).*/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
+}
